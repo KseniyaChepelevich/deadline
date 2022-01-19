2f73e2ba4618a78df13b050fbec0a7e8962b9b22
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-01-19 23:07:36 +0300

    updated DbInteractionDbUtils

---
+++ src/test/java/ru/netology/DbInteractionDbUtils.java
@@ -0,0 +1,81 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.mode.User;
+
+import java.sql.DriverManager;
+
+public class DbInteractionDbUtils {
+
+//    @AfterAll
+//    void deletingDataFromTheDb() {
+//        var runner = new QueryRunner();
+//
+//        var cardTrasactionsSQL = "DROP TABLE IF EXISTS card_transactions;";
+//        var authCodesSQL = "DROP TABLE IF EXISTS auth_codes;";
+//        var cardsSQL = "authCodesSQLDROP TABLE IF EXISTS cards;";
+//        var userSQL = "DROP TABLE IF EXISTS users;";
+//        var conn = DriverManager.getConnection(
+//                       "jdbc:mysql://localhost:3306/app-db", "app", "mypass"
+//                );
+//
+//
+//        {
+//
+//            runner.update(conn, cardTrasactionsSQL);
+//            runner.update(conn, authCodesSQL);
+//            runner.update(conn, cardsSQL);
+//            runner.update(conn, userSQL);
+//        }
+//
+//    }
+    @Test
+    @BeforeEach
+    @SneakyThrows
+    void setUp() {
+        var faker = new Faker();
+        var runner = new QueryRunner();
+        var dataSQL = "INSERT INTO users(login, password) VALUES (?, ?);";
+
+        try (
+                var conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app-db", "app", "mypass"
+                );
+
+        ) {
+            // обычная вставка
+            runner.update(conn, dataSQL, faker.name().username(), "pass");
+            runner.update(conn, dataSQL, faker.name().username(), "pass");
+        }
+    }
+
+//    @Test
+//    @SneakyThrows
+//    void stubTest() {
+//        var countSQL = "SELECT COUNT(*) FROM users;";
+//        var usersSQL = "SELECT * FROM users;";
+//        var runner = new QueryRunner();
+//
+//        try (
+//                var conn = DriverManager.getConnection(
+//                        "jdbc:mysql://localhost:3306/app-db", "app", "mypass"
+//                );
+//        ) {
+//            var count = runner.query(conn, countSQL, new ScalarHandler<>());
+//            System.out.println(count);
+//            var first = runner.query(conn, usersSQL, new BeanHandler<>(User.class));
+//            System.out.println(first);
+//            var all = runner.query(conn, usersSQL, new BeanListHandler<>(User.class));
+//            System.out.println(all);
+//        }
+//    }
+}
